0bda8a3db2536c2d9cbb75ded6caa5298be1e2ee
buddini96 <buddinikl1996@gmail.com>
2024-12-23 19:25:52 +0530

    most popular completed

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Hero from "./Hero/Hero";
 import WhyChoose from "./WhyChoose/WhyChoose";
 import AnalyticFeatures from "./AnalyticFeatures/AnalyticFeatures";
+import MostPopular from "./MostPopular/MostPopular";
 
 const Home = () => {
   return (
-    <div className="overflow-hidden bg-[url('/images/background.png')] bg-cover bg-center">
+    <div className="overflow-hidden bg-[url('/images/Frame.png')] bg-contain bg-center bg-repeat">
       <Hero />
       <WhyChoose />
       <AnalyticFeatures />
+      <MostPopular />
     </div>
   );
 };
---
+++ src/app/components/Home/MostPopular/MostPopular.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import MostPopularCard from "./MostPopularCard";
+
+const MostPopular = () => {
+  const cardData = [
+    {
+      image: "/images/img1.png",
+      title: "Haikyuu",
+      description:
+        "is a Japanese manga series witten and illustrated by Haruichi Furudate.",
+    },
+    {
+      image: "/images/img2.png",
+      title: "Demon Slayer",
+      description:
+        "Kimetsu no Yaiba is a popular anime based on a manga by Kayoharu Gotouge.",
+    },
+    {
+      image: "/images/img3.png",
+      title: "Naruto",
+      description:
+        "is a Japanese manga series written and illustrated by Masashi Kishimoto.",
+    },
+    {
+      image: "/images/img5.png",
+      title: "Attack on Titan",
+      description:
+        "A manga by Hajime Isayama, where humanity battles giant creatures called Titans.",
+    },
+  ];
+  return (
+    <div className="pt-24 pb-16">
+      <h1 className="mt-6 text-2xl md:text-3xl capitalize font-bold text-center">
+        Most Popular
+      </h1>
+      <div className="mt-20 grid w-[90%] mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-12">
+        {cardData.map((data) => (
+          <MostPopularCard
+            key={data.title}
+            image={data.image}
+            title={data.title}
+            description={data.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MostPopular;
